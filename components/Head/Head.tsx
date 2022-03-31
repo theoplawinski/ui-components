e3a47c9234372f8d762597ea1d13c21f5449fab1
theoplawinski <plawinski.theo@gmail.com>
2022-03-31 20:10:00 +0200

    Updated favicons & meta

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -1,29 +1,41 @@
 import NextHead from "next/head";
 
-const title = "UI Components"
-const description = "Collection of reusable UI Components"
-const url = "https://theoplawinski.github.io/ui-components/"
-const ogImage = "https://theoplawinski.github.io/ui-components/og-image.jpg"
+const title = "UI Components";
+const description = "Collection of reusable UI Components";
+const url = "https://theoplawinski.github.io/ui-components/";
+const ogImage = "https://theoplawinski.github.io/ui-components/og-image.jpg";
 
 const Head = () => {
   return (
     <NextHead>
+      {/* General */}
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
+
       <meta name="og:title" content={title} />
       <meta name="description" content={description} />
       <meta name="og:description" content={description} />
-      <meta name="twitter:image" content={ogImage} />
       <meta name="og:image" content={ogImage} />
       <meta name="og:url" content={url} />
+      <meta name="author" content="Theo Plawinski" />
+
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@theoplawinski" />
-      <meta name="author" content="Theo Plawinski" />
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta name="theme-color" content="#fff" />
-      <link rel="shortcut icon" href="favicon.ico" />
+      <meta name="twitter:creator" content="@theoplawinski" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={ogImage} />
+
+      {/* Favicon */}
       <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+      <link rel="shortcut icon" href="favicon.ico" />
+      <link rel="mask-icon" href="safari-pinned-tab.svg" color="#ff424d" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
     </NextHead>
   );
 };
