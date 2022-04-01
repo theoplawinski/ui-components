902831a330024a883596fc437ddb9aed07d8d69b
theoplawinski <hello@theoplawinski.com>
2022-04-01 14:14:15 +0200

    Added footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,29 @@
 import css from './Footer.module.scss'
 import { merge } from '../../helpers/merge'
+import Container from '../container/Container'
 
 interface IProps {
   className?: string
 }
 
 const Footer = (props: IProps) => {
-  return <footer className={merge([css.root, props.className])}>Footer</footer>
+  return (
+    <footer className={merge([css.root, props.className])}>
+      <Container>
+        <p className={css.text}>
+          Built by{' '}
+          <a
+            className={css.externalLink}
+            href="https://theoplawinski.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Theo Plawinski
+          </a>
+        </p>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
