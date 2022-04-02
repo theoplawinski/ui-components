e00af2149717067e6a5025f22f9900d933828d82
theoplawinski <plawinski.theo@gmail.com>
2022-04-02 23:38:05 +0200

    Fixed file name error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../src/index.scss'
 import '../styles.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
-import WindowHelper from '../src/helpers/windowHelper'
+import WindowHelper from '../src/helpers/WindowHelper'
 
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
