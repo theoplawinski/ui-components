36802ad18f794980193cc7393265ea5c5d26ee23
theoplawinski <36503652+theoplawinski@users.noreply.github.com>
2022-04-13 22:11:51 +0000

    Deploying to gh-pages from @ theoplawinski/ui-components@aabdbf310ecf8079f1fafae813efcf7684431671 🚀

--- _next/static/fFsL8un9UsMPmmTDBbS5M/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/49-27b0e7401a957aba.js","static/css/556851cc2d7c4591.css","static/chunks/pages/index-c356965f5954388c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
